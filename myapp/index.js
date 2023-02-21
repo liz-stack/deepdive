c91966ac80dde0fd80eddaf4acf3bf82ea29036e
liz <80471845+liz-stack@users.noreply.github.com>
2023-02-21 23:38:07 +0900

    first commit

---
+++ myapp/index.js
@@ -0,0 +1,12 @@
+
+const arr = [1, 2, 3]
+arr.forEach(console.log)
+// arr.forEach(alert) 
+
+//예제 4-100
+console.log(score) //undefined
+
+score = 80 //값 할당
+var score; //변수 선언
+
+console.log(score) // 80
